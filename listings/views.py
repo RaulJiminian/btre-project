4684f4703527d12af60422338d9eeb06a530ff05
Raul Jiminian <raul.e.jiminian@gmail.com>
2022-01-23 19:30:53 -0500

    generate content for search.html

--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from .choices import price_choices, bedroom_choices, state_choices
+
 
 from .models import Listing
 
@@ -24,8 +26,16 @@ def listing(request, listing_id):
     context = {
         'listing': listing
     }
+
     return render(request, 'listings/listing.html', context)
 
 
 def search(request):
-    return render(request, 'listings/search.html')
+
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices
+    }
+
+    return render(request, 'listings/search.html', context)
